622bc42744f7dc68f1fa4d0c0caa0d96405df355
Claire Lin <clairelin135@gmail.com>
2019-04-05 16:16:45 -0700

    update requests, firebase

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask, request, render_template
+from firebase import firebase
 
 app = Flask(__name__)
-
-keys = ['key1', 'key2', 'key3', 'key4']
-request_vals = {}
-for k in keys:
-    request_vals[k] = 'Not home'
+firebase = firebase.FirebaseApplication('https://smartkeyrack.firebaseio.com/', None)
 
 @app.route('/')
 def index():
-    return render_template('index.html', key1 = request_vals[keys[0]], key2 = request_vals[keys[1]], key3 = request_vals[keys[2]], key4 = request_vals[keys[3]])
+    keys = firebase.get('/keys', None)
+    key1 = keys['key1']
+    key2 = keys['key2']
+    key3 = keys['key3']
+    key4 = keys['key4']
+    return render_template('index.html', key1 = key1, key2 = key2, key3 = key3, key4 = key4)
 
 @app.route('/update')
 def update():
+    keys = ['key1', 'key2', 'key3', 'key4']
     for k in keys:
-        if request.args.get(k):
-            request_vals[k] = request.args.get(k)
+        val = request.args.get(k)
+        if val:
+            #firebase.put('/keys/' + k, val, {'print': 'silent'})
+            #firebase.put('/keys/' + k, val, {'print': 'silent'})
+            firebase.patch('/keys/', {k: val})
+    return 'lol'
 
 if __name__ == '__main__':
     app.run()
