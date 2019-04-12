253d4ab04ff81c3db900a6bb28c23bf215f4db18
Claire Lin <clairelin135@gmail.com>
2019-04-12 11:12:06 -0700

    attempt to add websocket

--- app.py
+++ app.py
@@ -1,29 +1,53 @@
 from flask import Flask, request, render_template
+from flask_socketio import SocketIO, emit
 from firebase import firebase
 
+'''
+import eventlet
+eventlet.monkey_patch()
+'''
+
 app = Flask(__name__)
+socketio = SocketIO(app, logger=True, engineio_logger=True)
 firebase = firebase.FirebaseApplication('https://smartkeyrack.firebaseio.com/', None)
+
+'''
+Payload Structure
+key_statuses = {
+    'key1': {
+        'name': 'Claire Lin',
+        'status': 'home'
+    }
+    ...
+}
+'''
+keys = ['key1', 'key2', 'key3', 'key4']
+key_statuses = firebase.get('/keys', None)
 
 @app.route('/')
 def index():
-    keys = firebase.get('/keys', None)
-    key1 = keys['key1']
-    key2 = keys['key2']
-    key3 = keys['key3']
-    key4 = keys['key4']
+    return render_template('index.html', **key_statuses)
 
-    return render_template('index.html', key1 = key1, key2 = key2, key3 = key3, key4 = key4)
+@socketio.on('connect')
+def on_connect():
+    pass
 
 @app.route('/update', methods=['POST'])
 def update():
     req_data = request.json
     if request.method == 'POST':
-        keys = ['key1', 'key2', 'key3', 'key4']
         for k in keys:
-            val = req_data.get(k, None)
-            if val:
-                firebase.patch('/keys/', {k: val})
-    return req_data or "None"
+            keyAttr = req_data.get(k, None)
+            if keyAttr:
+                for attr in keyAttr:
+                    key_statuses[k][attr] = keyAttr[attr]
+            #firebase.patch('/keys/', {k: key_statuses[k]})
+        payload = {
+            'hi': 'hello'
+        }
+        socketio.emit('render keys', payload)
+        socketio.sleep(0)
+    return "None"
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, host='0.0.0.0')
