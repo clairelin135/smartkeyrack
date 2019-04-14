0a4686620255ae26f144e44a7fa6dd50c347f31b
Claire Lin <clairelin135@gmail.com>
2019-04-13 17:50:00 -0700

    revert w/o websocket

--- app.py
+++ app.py
@@ -1,65 +1,29 @@
 from flask import Flask, request, render_template
-from flask_socketio import SocketIO, emit
 from firebase import firebase
 
-'''
-import eventlet
-eventlet.monkey_patch()
-'''
-'''
-import gevent.monkey
-gevent.monkey.patch_all()
-'''
 app = Flask(__name__)
-socketio = SocketIO(app, logger=True, engineio_logger=True)
 firebase = firebase.FirebaseApplication('https://smartkeyrack.firebaseio.com/', None)
-
-'''
-Payload Structure
-key_statuses = {
-    'key1': {
-        'name': 'Claire Lin',
-        'status': 'home'
-    }
-    ...
-}
-'''
-keys = ['key1', 'key2', 'key3', 'key4']
-key_statuses = firebase.get('/keys', None)
-
-clients = []
 
 @app.route('/')
 def index():
-    return render_template('index.html', **key_statuses)
+    keys = firebase.get('/keys', None)
+    key1 = keys['key1']
+    key2 = keys['key2']
+    key3 = keys['key3']
+    key4 = keys['key4']
 
-@socketio.on('connected')
-def connected():
-    print('connected' + request.namespace)
-    clients.append(request.namespace)
-
-@socketio.on('disconnect')
-def disconnect():
-    print('disconnected' + request.namespace)
-    clients.remove(request.namespace)
+    return render_template('index.html', key1 = key1, key2 = key2, key3 = key3, key4 = key4)
 
 @app.route('/update', methods=['POST'])
 def update():
     req_data = request.json
     if request.method == 'POST':
+        keys = ['key1', 'key2', 'key3', 'key4']
         for k in keys:
-            keyAttr = req_data.get(k, None)
-            if keyAttr:
-                for attr in keyAttr:
-                    key_statuses[k][attr] = keyAttr[attr]
-            #firebase.patch('/keys/', {k: key_statuses[k]})
-        payload = {
-            'hi': 'hello'
-        }
-        # for i in range(len(clients)):
-        #     clients[i].emit('render keys', payload)
-        socketio.emit('render keys', payload)
-    return "None"
+            val = req_data.get(k, None)
+            if val:
+                firebase.patch('/keys/', {k: val})
+    return req_data or "None"
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
+    app.run()
