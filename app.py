b44ec405f6587cd09947bcaa95fed7de580c84db
Tammy Truong <tammytruong@berkeley.edu>
2019-04-15 12:50:48 -0700

    theo's changes for the edit button

--- app.py
+++ app.py
@@ -15,6 +15,26 @@ def index():
 
     return render_template('index.html', key1 = key1, key2 = key2, key3 = key3, key4 = key4)
 
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    keys = firebase.get('/keys', None)
+    key1 = keys['key1']
+    key2 = keys['key2']
+    key3 = keys['key3']
+    key4 = keys['key4']
+    if request.method == 'POST':
+        keys = ['key1', 'key2', 'key3', 'key4']
+        for k in keys:
+            newname = request.form[k]
+            if newname:
+                firebase.patch('/keys/', {"name": newname})
+                print(newname)
+    return render_template('index.html', key1=key1, key2=key2, key3=key3, key4=key4)
+
 @app.route('/update', methods=['POST'])
 def update():
     req_data = request.json
